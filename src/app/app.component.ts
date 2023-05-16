6b65b649a090955f8bd826bcb61b42f0e84280ad
Pablo Vignolo <vignolopbl@gmail.com>
2023-05-16 17:08:57 -0300

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera app de Angular';
+
 }
